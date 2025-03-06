65257a73fb2d05eaa3779a79336dd7b1f3f63adc
Astamp9 <andrew_de_stamps@yahoo.com>
2025-03-06 17:11:46 -0500

    messing with the battle

--- src/components/Combatant/Combatant.jsx
+++ src/components/Combatant/Combatant.jsx
@@ -1,19 +1,25 @@
-import './Combatant.css'
+import './Combatant.css';
 
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
-import { useState, useEffect, useContext } from 'react'
-
-export default function Combatant(pokemonData) {
-    const [pokemon, setPokemon] = useState(pokemonData);
+export default function Combatant({ player, pokemonData, currentHP }) {
+    const maxHP = pokemonData.stats[0]?.base_stat;
 
     return (
-        <>
-            {pokemon ? (
-                <div>{pokemon.pokemonData.name}</div>
-                /* <img src={pokemon.pokemonData.sprites.front_default} /> */
-            ): (
-                <div>loading</div>
+        <div className="combatant">
+            {player === 'one' ? (
+                <div>
+                    <h2>Player 1</h2> 
+                    <p>{pokemonData.name}</p>
+                    <p>{currentHP}/{maxHP} HP</p>
+                    <img className="idle-front" src={pokemonData?.sprites?.other?.showdown?.front_default} alt={`${pokemonData.name} front`} />
+                </div>
+            ) : (
+                <div>
+                    <h2>Player 2</h2>
+                    <img className="idle-back" src={pokemonData?.sprites?.other?.showdown?.back_default} alt={`${pokemonData.name} back`} />
+                    <p>{pokemonData.name}</p>
+                    <p>{currentHP}/{maxHP} HP</p>
+                </div>
             )}
-        </>
-    )
+        </div>
+    );
 }
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setupTests.js",
   },
 });
+
