9a0e284ce6bc875dccb87548a716d84ad6486086
Landon-Spencer <landon.r.spencer@gmail.com>
2025-03-07 08:35:32 -0700

    added toggle switch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,11 +10,13 @@ import PokemonDetails from '../PokemonDetails/PokemonDetails.jsx';
 import PokemonTypeList from '../PokemonTypeList/PokemonTypeList.jsx';
 import TypeDetails from '../TypeDetails/TypeDetails.jsx';
 
-import colorMode from '../../hooks/colorMode.js';
+// import colorMode from '../../hooks/colorMode.js';
+import CustomSwitch from '../../hooks/CustomSwitch.jsx';
+
 
 function App() {
   const navigate = useNavigate();
-  const { mode, toggleMode } = colorMode();
+  // const { mode, toggleMode } = colorMode();
 
   const [details, setDetails] = useState({});
   const value = { details, setDetails };
@@ -74,7 +76,8 @@ function App() {
       <PokemonTypesContext.Provider value={typeValue}>
         <PokemonContext.Provider value={value}>
           <nav>
-            <button onClick={toggleMode}>Toggle Dark Mode</button>
+            <CustomSwitch />
+            {/* <button onClick={toggleMode}>Toggle Dark Mode</button> */}
             <button onClick={() => navigate('/')}>Pokemon Types</button>
             <button onClick={() => { fetchRandomPokemon(); navigate('/battle'); }}>New Battle</button>
 
